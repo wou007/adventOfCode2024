964ba7ac3ca178affb28bad911106ecc66d4a847
wou007 <58426499+wou007@users.noreply.github.com>
2024-12-01 13:04:18 +0100

    Update Day1.py
    Finished day 1

--- Day1.py
+++ Day1.py
@@ -3,13 +3,38 @@ import re
 def Part1(f):
     res = 0
 
+    left = []
+    right = []
+
     for l in f.readlines():
-        res+=1
+        m = re.match('(\d+)   (\d+)',l)
+        left.append(int(m[1]))
+        right.append(int(m[2]))
+
+    left.sort()
+    right.sort()
+
+    for i in range(len(left)):
+        res += abs(left[i]-right[i])
 
     return res
 
 def Part2(f):
     res = 0
+
+    left = []
+    right = []
+
+    for l in f.readlines():
+        m = re.match('(\d+)   (\d+)',l)
+        left.append(int(m[1]))
+        right.append(int(m[2]))
+
+    left.sort()
+    right.sort()
+
+    for i in left:
+        res += right.count(i) * i
 
     return res
 
