a63b3d2bb950b5586b87b5abab7a224abcc6a6b8
wou007 <58426499+wou007@users.noreply.github.com>
2024-12-08 22:48:34 +0100

    8 part 1 and part of part 2

--- Day8.py
+++ Day8.py
@@ -4,13 +4,15 @@ import HelperFunctions
 day_number = 8
 
 def Part1(f):
-    res = 0
 
     keys = {}
 
     y = 0
 
+    width = 0
+
     for l in f.splitlines():
+        width = len(l)
         for x in range(len(l)):
             if l[x] != '.':
                 if not l[x] in keys:
@@ -37,24 +39,81 @@ def Part1(f):
                     else:
                         x1 = a[1] + deltaX
                         x2 = b[1] - deltaX
-                    if a[0] < b[1]:
+                    if a[0] < b[0]:
                         y1 = a[0] - deltaY
                         y2 = b[0] + deltaY
                     else:
                         y1 = a[0] + deltaY
                         y2 = b[0] - deltaY
- 
-                    news.append([x1,y1])
-                    news.append([x2,y2])
 
-    return res
+                    if not [x1,y1] in news:
+                        if x1 >= 0 and x1 < width and y1 >= 0 and y1 < y:
+                            news.append([x1,y1])
+                    if not [x2,y2] in news:
+                        if x2 >= 0 and x2 < width and y2 >= 0 and y2 < y:
+                            news.append([x2,y2])
+
+    return len(news)
 
 def Part2(f):
-    res = 0
+    keys = {}
 
-    
+    y = 0
 
-    return res
+    width = 0
+
+    for l in f.splitlines():
+        width = len(l)
+        for x in range(len(l)):
+            if l[x] != '.':
+                if not l[x] in keys:
+                    keys[l[x]] = []
+                keys[l[x]].append([y,x])
+        y+=1
+
+    news = []
+
+    for k in keys:
+        ant = keys[k]
+        
+        for a in ant:
+            if len(ant) > 1:
+                news.append([a[1],a[0]])
+            for b in ant:
+                if a != b:
+                    x1 = a[1] 
+                    y1 = a[0]
+                    x2 = b[1]
+                    y2 = b[0]
+                    do = True
+                    deltaX = abs(a[1]-b[1])
+                    deltaY = abs(a[0]-b[0])
+                    for i in range(1000):
+                        do = False
+                        if a[1] < b[1]: #X
+                            x1 -= deltaX
+                            x2 += deltaX
+                        else:
+                            x1 += deltaX
+                            x2 -= deltaX
+                        if a[0] < b[0]:
+                            y1 -= deltaY
+                            y2 += deltaY
+                        else:
+                            y1 += deltaY
+                            y2 -= deltaY
+
+                        
+                        if x1 >= 0 and x1 < width and y1 >= 0 and y1 < y:
+                            if not [x1,y1] in news:
+                                news.append([x1,y1])
+                            do = True
+                        if x2 >= 0 and x2 < width and y2 >= 0 and y2 < y:
+                            if not [x2,y2] in news:
+                                news.append([x2,y2])
+                            do = True
+
+    return len(news)
 
 if __name__=="__main__":
     input = HelperFunctions.ReadInput(day_number)
