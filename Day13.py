04e2f7c31ab54fc53935e53adee4422b6695b407
wou007 <58426499+wou007@users.noreply.github.com>
2024-12-13 13:42:56 +0100

    Day13 part 1 and 2 in a while

---
+++ Day13.py
@@ -0,0 +1,69 @@
+import re
+import HelperFunctions
+
+day_number = 13
+
+def FindCost(a,b,prize):
+    aCount = 0
+    bCount = 0
+    
+    bCount = int(prize[0] / b[0]) + 1
+    while bCount > 0:
+        x = aCount * a[0] + bCount * b[0]
+        y = aCount * a[1] + bCount * b[1]
+        if x == prize[0] and y == prize[1]:
+            return aCount * 3 + bCount
+        elif x > prize[0] or y > prize[1]:
+            bCount-=1
+        elif x < prize[0] or y < prize[1]:
+            aCount+=1
+    return 0
+
+def Part1(f):
+    res = 0
+
+    butA = None
+    butB = None
+    prize = None
+    for l in f.splitlines():
+        a = re.match(r'Button A: X\+(\d+), Y\+(\d+)',l)
+        b = re.match(r'Button B: X\+(\d+), Y\+(\d+)',l)
+        p = re.match(r'Prize: X=(\d+), Y=(\d+)',l) 
+        
+        if a:
+            butA = [int(a[1]),int(a[2])]
+        if b:
+            butB = [int(b[1]),int(b[2])]
+        if p:
+            prize = [int(p[1]),int(p[2])]
+            res += FindCost(butA,butB,prize)
+
+    return res
+
+def Part2(f):
+    res = 0
+
+    butA = None
+    butB = None
+    prize = None
+    for l in f.splitlines():
+        a = re.match(r'Button A: X\+(\d+), Y\+(\d+)',l)
+        b = re.match(r'Button B: X\+(\d+), Y\+(\d+)',l)
+        p = re.match(r'Prize: X=(\d+), Y=(\d+)',l) 
+        
+        if a:
+            butA = [int(a[1]),int(a[2])]
+        if b:
+            butB = [int(b[1]),int(b[2])]
+        if p:
+            prize = [int(p[1]) + 10000000000000,int(p[2]) + 10000000000000]
+            res += FindCost(butA,butB,prize)
+            print(res)
+
+    return res
+
+if __name__=="__main__":
+    input = HelperFunctions.ReadInput(day_number)
+    if input != None:
+        print(f'Part 1: {Part1(input)}')
+        print(f'Part 2: {Part2(input)}')
